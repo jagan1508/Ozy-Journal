969252b9ce8713cf2dd0f4021d3d455e4bbc7ac8
jagan1508 <jaganathbabu025@gmail.com>
2026-04-08 08:00:13 +0530

    basic login logic added

--- backend/app/apis/routes/users.py
+++ backend/app/apis/routes/users.py
@@ -1,13 +1,18 @@
 from fastapi import APIRouter, Depends,HTTPException
-from app.schema.user import UserCreate,UserLogin
+from app.schema.user import UserCreate,UserLogin,UserOut
 from app.db.session import get_db
 from sqlalchemy.orm import Session
-from app.service.user import create_user
+from app.service.user import create_user,get_all_users,authenticate_user
 
 router=APIRouter()
 @router.get("/")
 async def read_root():
     return {"Message": "User API is working"}
+
+@router.get("/all",response_model=list[UserOut])
+async def get_users(db: Session = Depends(get_db)):
+    return get_all_users(db)
+    
 
 @router.post("/register")
 async def register_user(user:UserCreate, db: Session = Depends(get_db)):
@@ -17,7 +22,12 @@ async def register_user(user:UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 
-@router.post("/login")
-async def login(user: UserLogin):
-    return {"message": "login logic here"}
+@router.post("/login",response_model=UserOut)
+async def login(user: UserLogin,db: Session = Depends(get_db)):
+    db_user=authenticate_user(db, user.email, user.password)
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+    return db_user
+
+
 
--- backend/app/service/user.py
+++ backend/app/service/user.py
@@ -1,4 +1,5 @@
 from app.models.user import User
+from sqlalchemy.orm import Session
 
 async def create_user(db, user_data):
     existing = db.query(User).filter(User.email == user_data.email).first()
@@ -16,3 +17,14 @@ async def create_user(db, user_data):
     db.commit()
     db.refresh(user)
     return user
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = db.query(User).filter(User.email == email).first()
+    if not user:
+        return None
+    if user.hashed_password != password:  # temp check
+        return None
+    return user
+
+def get_all_users(db:Session):
+    return db.query(User).all()
